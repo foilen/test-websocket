fe1d01e36475997a9a6970bc97d09cba420f3ff2
Simon Levesque <simon@foilen.com>
2019-02-02 20:18:47 -0500

    Base

---
+++ src/main/java/com/foilen/websocket/TestWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.foilen.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebsocketApplication.class, args);
+	}
+
+}
+
